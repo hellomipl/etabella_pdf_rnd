39c45cd9f6331aeee72bf1747ef47f4bd4fcc10d
Rajendra-mipl <rajendrapatel.mipl@gmail.com>
2024-06-05 09:48:53 +0530

    kj

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { CUSTOM_ELEMENTS_SCHEMA, ChangeDetectorRef, Component, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HighlightEditorAnnotation, InkEditorAnnotation, NgxExtendedPdfViewerComponent, NgxExtendedPdfViewerModule, NgxExtendedPdfViewerService } from 'ngx-extended-pdf-viewer';
+import { FreeTextEditorAnnotation, HighlightEditorAnnotation, InkEditorAnnotation, NgxExtendedPdfViewerComponent, NgxExtendedPdfViewerModule, NgxExtendedPdfViewerService, PdfHighlightEditorComponent } from 'ngx-extended-pdf-viewer';
 import { IPDFViewerApplication } from 'ngx-extended-pdf-viewer';
 
 export interface Annotation {
@@ -21,13 +21,14 @@ export interface Annotation {
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  @ViewChild('pdfViewer') pdfViewer!: PdfHighlightEditorComponent;
   title = 'pdfmanagement';
   pdfSrc: string = './assets/demo.pdf';
   pageViewMode: any = 'multiple';
   pageRotaion: any = 0;
 
-  @ViewChild(NgxExtendedPdfViewerComponent, { static: false })
-  private pdfViewer!: NgxExtendedPdfViewerComponent;
+  // @ViewChild(NgxExtendedPdfViewerComponent, { static: false })
+  // private pdfViewer!: NgxExtendedPdfViewerComponent;
   currentPage: number = 1;
   constructor(private pdfService: NgxExtendedPdfViewerService, private cdr: ChangeDetectorRef) {
 
@@ -165,88 +166,149 @@ export class AppComponent {
 
   public addDrawing(): void {
     debugger;
-    const drawing: any = {
-      "annotationType": 9,
-      "color": [
-        128,
-        235,
-        255
-      ],
-      "opacity": 1,
-      "thickness": 12,
-      "quadPoints": [
-        122.14229337630734,
-        726.5100677120456,
-        505.485215956165,
-        726.5100677120456,
-        122.14229337630734,
-        739.7132038488952,
-        505.485215956165,
-        739.7132038488952,
-        89.90415511592741,
-        712.6974117961511,
-        211.15495128016317,
-        712.6974117961511,
-        89.90415511592741,
-        725.9005479330007,
-        211.15495128016317,
-        725.9005479330007
-      ],
-      "outlines": [
-        [
-          121.499,
-          726.8144,
-          121.499,
-          740.6232,
-          506.1069999999999,
-          740.6232,
-          506.1069999999999,
-          725.6356000000001,
-          211.76049999999998,
-          725.6356000000001,
-          211.76049999999998,
-          711.8268,
-          89.25,
-          711.8268,
-          89.25,
-          726.8144
-        ]
-      ],
-      "pageIndex": 0,
-      "rect": [
-        89.25,
-        711.8268,
-        506.1069999999999,
-        740.6232
-      ],
-      "rotation": 0,
-      "structTreeParentId": null
-    }
+    this.pdfViewer;
 
+    // const selectedText = window.getSelection();
+    // if (selectedText !== null) {
+    //   const text = selectedText.toString();
+    //   if (text) {
+    //     this.pdfViewer.addHighlight({ text });
+    //   }
+    // }
 
-    // const x = 400 * Math.random();
-    // const y = 350 + 500 * Math.random();
-    // const drawing: HighlightEditorAnnotation = { //InkEditorAnnotation
-    //   annotationType: 9,
-    //   color: [Math.floor(Math.random() * 255), Math.floor(Math.random() * 255), Math.floor(Math.random() * 255)],
-    //   // thickness: Math.random() * 10,
-    //   // opacity: 1,
-    //   // paths: [
-    //   //   {
-    //   //     bezier: [x + 0.5, y, x + 0.5, y + 44, x + 44, y + 66, x + 88, y + 44],
-    //   //     points: [x + 0.5, y, x + 0.5, y + 44],
-    //   //   },
-    //   // ],
-    //   pageIndex: 0,
-    //   rect: [
-    //     94.158216,
-    //     591.059592,
-    //     508.227192,
-    //     617.499648
-    //   ], //[x, y, x + 100, y + 66],
-    //   rotation: 0,
-    // };
-    this.pdfService.addEditorAnnotation(drawing);
+    // return;
+    // const drawing:any = {
+    //   "annotationType": 9,
+    //   "color": [
+    //     255,
+    //     255,
+    //     152
+    //   ],
+    //   "opacity": 1,
+    //   "thickness": 12,
+    //   "quadPoints": [
+    //     134.35915854669386,
+    //     726.5100677120456,
+    //     228.06898024774367,
+    //     726.5100677120456,
+    //     134.35915854669386,
+    //     739.7132038488952,
+    //     228.06898024774367,
+    //     739.7132038488952
+    //   ],
+    //   "outlines": [
+    //     [
+    //       133.75599999999997,
+    //       725.6356,
+    //       133.75599999999997,
+    //       740.6232,
+    //       228.71799999999996,
+    //       740.6232,
+    //       228.71799999999996,
+    //       725.6356
+    //     ]
+    //   ],
+    //   "pageIndex": 0,
+    //   "rect": [
+    //     133.75599999999997,
+    //     725.6356,
+    //     228.71799999999996,
+    //     740.6232
+    //   ],
+    //   "rotation": 0,
+    //   "structTreeParentId": null
+    // }
+
+    /*
+        const x = Math.round(Math.random() * 400);
+        const y = Math.round(350 + Math.random() * 500);
+        const fontSize = Math.round(Math.random() * 30 + 10);
+        const height = fontSize * 1.75;
+        const width = fontSize * 5.8;
+        const textEditorAnnotation: FreeTextEditorAnnotation = {
+          annotationType: 3,
+          color: [Math.floor(Math.random() * 255), Math.floor(Math.random() * 255), Math.floor(Math.random() * 255)],
+          fontSize: fontSize,
+          value: 'Hello world!',
+          pageIndex: 0,
+          rect: [
+            x,
+            y,
+            x + width,
+            y + height
+          ],
+          rotation: 0,
+        };
+        console.log(textEditorAnnotation);
+        console.log('Before update');
+        this.pdfService.addEditorAnnotation(textEditorAnnotation);
+        console.log('After update');
+        let anno = this.pdfService.getSerializedAnnotations();
+        if (anno) {
+          console.log(anno[0]);
+        }*/
+        const x = 400 * Math.random();
+        const y = 350 + 500 * Math.random();
+        const drawing: InkEditorAnnotation = {
+          annotationType: 15,
+          color: [Math.floor(Math.random() * 255), Math.floor(Math.random() * 255), Math.floor(Math.random() * 255)],
+          thickness:  10,
+          opacity: 1,
+          paths: [
+            {
+              bezier: [x + 0.5, y, x + 0.5, y + 44, x + 44, y + 66, x + 88, y + 44],
+              points: [x + 0.5, y, x + 0.5, y + 44],
+            },
+          ],
+          pageIndex: 0,
+          rect: [x, y, x + 100, y + 66],
+          rotation: 0,
+        };
+        this.pdfService.addEditorAnnotation(drawing);
+   /* const x = 400 * Math.random();
+    const y = 350 + 500 * Math.random();
+    const drawing: InkEditorAnnotation = {
+      annotationType: 15,
+      color: [Math.floor(Math.random() * 255), Math.floor(Math.random() * 255), Math.floor(Math.random() * 255)],
+      thickness: Math.random() * 10,
+      opacity: 1,
+      paths: [
+        {
+          bezier: [x + 0.5, y, x + 0.5, y + 44, x + 44, y + 66, x + 88, y + 44],
+          points: [x + 0.5, y, x + 0.5, y + 44],
+        },
+      ],
+      pageIndex: 0,
+      rect: [x, y, x + 100, y + 66],
+      rotation: 0,
+    };
+    this.pdfService.addEditorAnnotation(drawing);*/
+    this.getSerializeAnnot();
   }
+
+
+
+  // public updateAnnotation(index: number, event: Event): void {
+  //   const textarea = event.target as HTMLTextAreaElement;
+  //   console.log('Before update');
+  //   let anno = this.pdfService.getSerializedAnnotations();
+  //   if (anno) {
+  //     console.log(anno[0]);
+  //   }
+  //   if (this.rawAnnotations) {
+  //     const value = textarea.value.replace(/\n/g, '');
+  //     this.rawAnnotations[index] = JSON.parse(value);
+
+  //     this.removeEditors();
+  //     for (const annotation of this.rawAnnotations) {
+  //       this.pdfViewerService.addEditorAnnotation(annotation);
+  //     }
+  //   }
+  //   console.log('After update');
+  //   anno = this.pdfViewerService.getSerializedAnnotations();
+  //   if (anno) {
+  //     console.log(anno[0]);
+  //   }
+  // }
 
 }
